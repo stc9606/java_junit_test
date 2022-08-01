0d63a6e12341e6c8e793f801e5eaa5ee7199d2e6
s_chuli <stc9606@naver.com>
2022-08-01 23:29:34 +0900

    junit Assertion

---
+++ src/main/java/me/log/junittest/StudyStatus.java
@@ -0,0 +1,5 @@
+package me.log.junittest;
+
+public enum StudyStatus {
+    DRAFT, STARTED, ENDED
+}
--- src/test/java/me/log/junittest/StudyTest.java
+++ src/test/java/me/log/junittest/StudyTest.java
@@ -2,22 +2,57 @@ package me.log.junittest;
 
 import org.junit.jupiter.api.*;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class StudyTest {
 
     @Test
-    void create() {
-        Study study = new Study();
-        assertNotNull(study);
+    @DisplayName("스터디 Timeout")
+    void timeout() {
+        // TODO 시간 초과 시 Test 종료
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            new Study(10);
+            Thread.sleep(300);
+            // TODO ThreadLocal
+        });
 
-        System.out.println("create");
+        // TODO 시간 초과 되어도 기댓 값이 종료 될 때 까지 대기
+        assertTimeout(Duration.ofMillis(100), () -> {
+            new Study(10);
+            Thread.sleep(300);
+        });
     }
 
     @Test
-    @Disabled // 테스트 disable 처
-    void create1() {
+    @DisplayName("스터디 예외 처리")
+    void exception() {
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+        String message = exception.getMessage();
+
+        assertEquals("limit은 0보다 커야 한다.", message);
+    }
+
+    @Test
+    @DisplayName("Assert 일괄 처리")
+    void assert_all() {
+        Study study = new Study(-10);
+
+        // TODO Assert 항목 일괄 처리
+        assertAll(
+                () -> assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(),
+                                () -> "스터디를 처음 만들면" + StudyStatus.DRAFT + "상태다."),
+                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
+        );
+    }
+
+    @Test
+    @DisplayName("모든 Test 실행 전 1번만 호출")
+    void create() {
         System.out.println("create");
     }
 
